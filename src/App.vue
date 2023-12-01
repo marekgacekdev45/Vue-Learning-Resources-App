f6c6499d6d010f3f4b0abc3d82608e9ca3e82f6b
gacmen45 <marekgacek45@gmail.com>
2023-12-01 06:40:56 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js docs',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'google',
+          description: 'Try google something',
+          link: 'https://google.com'
+        },
+      ],
+    };
+  },
+};
+</script>
